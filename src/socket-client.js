b430acf3e5972f717c441c804b86e3ba6a01bce2
Justin Chacon <justin@Justins-MacBook-Air-2.local>
2024-10-06 00:16:49 -0400

    Scoket fixes

--- src/socket-client.js
+++ src/socket-client.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-const socket = io(process.env.REACT_APP_SOCKET_URL || 'http://localhost:3000')
+const socket = io(process.env.REACT_APP_SOCKET_URL)
 
 export default socket
